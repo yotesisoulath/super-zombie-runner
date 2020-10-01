a0bded4684c098a7fb4e155d5a9a8b564275c046
Yote Sisoulath <yotesisoulath@gmail.com>
2020-10-01 07:02:34 -0400

    Add empty project

---
+++ SuperZombieRunner/Assets/Scripts/TiledBackground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiledBackground : MonoBehaviour
+{
+    public int textureSize = 32;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
